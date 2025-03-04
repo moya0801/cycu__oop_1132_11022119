73ac455170f6c2a71074594d1f3f50b6b4455fb5
moya0801 <howard19801016@gmail.com>
2025-03-04 20:22:55 +0800

    Create hw3.py

---
+++ 20250304/hw3.py
@@ -0,0 +1,22 @@
+def absolute_value_correct(x):
+    if x < 0:
+        return -x
+    else:
+        return x
+
+def absolute_value_correct(x):
+    if x < 0:
+        return -x
+    else:
+        return x
+
+def is_divisible(x, y):
+    return x % y == 0
+
+import math
+
+def distance(x1, y1, x2, y2):
+    return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+
+# 測試範例
+print(distance(1, 2, 4, 6))  # 輸出: 5.0
